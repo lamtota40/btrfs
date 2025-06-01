56d47de38f0152c742c02df709d8f4e67b2127cb
lamtota40 <lamtota40@gmail.com>
2025-06-01 10:40:36 +0700

    Update snapstore.sh

--- snapstore.sh
+++ snapstore.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
-mount_btrfs()
 
+mount_btrfs(){
 sudo mkdir -p /mnt/btrfs
-sudo mount -o subvolid=0 /dev/sda1 /mnt/btrfs
+
+for DEV in $(lsblk -pnlo NAME,FSTYPE | awk '$2=="btrfs"{print $1}'); do
+        echo "🔍 Mencoba mount $DEV -o subvol=0 ke /mnt/btrfs"
+        if sudo mount -o subvol=0 "$DEV" /mnt/btrfs 2>/dev/null; then
+            echo "✅ Berhasil mount $DEV ke /mnt/btrfs dengan subvol=0"
+            return 0
+        else
+            echo "❌ Gagal mount $DEV"
+        fi
+    done
+return 1
+}
+
+mount_btrfs
 sudo btrfs subvolume snapshot -r /mnt/btrfs/@ /mnt/btrfs/@_backup
+
+
 sudo btrfs send /mnt/btrfs/@_backup | gzip -c > btrfs-backup.img.gz
 #backup tanpa kompresi
 sudo btrfs send /mnt/btrfs/@_backup > btrfs-backup.img
@@ -12,4 +27,5 @@ sudo btrfs send /mnt/btrfs/@_backup > /mnt/sda2/btrfs-sda1-backup.img
 sudo btrfs send /mnt/btrfs/@_backup | gzip -c > /mnt/sda2/btrfs-sda1-backup.img.gz
 
 
-#restore dari GRML
+echo "Silakan tekan [ENTER] untuk melanjutkan reboot atau CTRL+C untuk membatalkan..."
+read
