e10f6ca441943fe0488fae462e0de9bb992ba405
lamtota40 <lamtota40@gmail.com>
2025-06-12 20:13:55 +0700

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+#snapshoot
+sudo mkdir -p /mnt/snap
+sudo mount -o subvolid=5 /dev/vda1 /mnt/snap
+sudo btrfs subvolume snapshot -r /mnt/snap/@ /mnt/snap/@_clean
+umount /mnt/snap
+rm -rf /mnt/snap
 
-sudo mkdir -p /mnt/btrfs
-sudo mount -o subvolid=5 /dev/vda1 /mnt/btrfs
-sudo btrfs subvolume snapshot -r /mnt/btrfs/@ /mnt/btrfs/@_clean
-umount /mnt/btrfs
-rm -rf /mnt/btrfs
+#!/bin/bash
+#restore
+sudo mkdir -p /mnt/res
+sudo mount -o subvolid=5 /dev/vda1 /mnt/res
+sudo btrfs subvolume delete /mnt/res/@
+sudo btrfs subvolume snapshot /mnt/res/@clean /mnt/res/@
+sudo umount /mnt/res
+sudo rm -rf /mnt/res
+reboot
