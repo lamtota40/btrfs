677f906ec7355ad37c16e9a0d0eed11211e0b48a
lamtota40 <lamtota40@gmail.com>
2025-06-12 20:00:39 +0700

    Create snap.sh

---
+++ snap.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo mkdir -p /mnt/btrfs
+sudo mount -o subvolid=5 /dev/vda1 /mnt/btrfs
+sudo btrfs subvolume snapshot -r /mnt/btrfs/@ /mnt/btrfs/@clean
+umount /mnt/btrfs
+rm -rf /mnt/btrfs
