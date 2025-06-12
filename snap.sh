d38adf5f49148becfea219a104f811451695cdb5
lamtota40 <lamtota40@gmail.com>
2025-06-12 21:03:19 +0700

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+sudo mkdir /mnt/vda1
+sudo mount -o subvol=@ /dev/vda1 /mnt/vda1
+sudo mount --bind /dev /mnt/vda1/dev
+sudo mount --bind /proc /mnt/vda1/proc
+sudo mount --bind /sys /mnt/vda1/sys
+chroot /mnt/vda1 /bin/bash -c "
+grub-editenv /boot/grub/grubenv list
+grub-editenv /boot/grub/grubenv unset next_entry
+update-grub
+"
+umount /mnt/vda1/dev /mnt/vda1/proc /mnt/vda1/sys
+umount /mnt/vda1
+rm -rf /mnt/vda1
+
 #snapshoot
 sudo mkdir -p /mnt/snap
 sudo mount -o subvolid=5 /dev/vda1 /mnt/snap
