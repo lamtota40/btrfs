c726a858ee27917e09090a9a4f54c7102ca17c00
lamtota40 <lamtota40@gmail.com>
2025-06-12 21:49:21 +0700

    Update snap.sh

--- snap.sh
+++ snap.sh
@@ -29,7 +29,7 @@ sudo mkdir -p /mnt/res
 sudo mount -o subvolid=5 /dev/vda1 /mnt/res
 sudo btrfs subvolume list /mnt/res
 sudo btrfs subvolume delete /mnt/res/@
-sudo btrfs subvolume snapshot /mnt/res/@clean /mnt/res/@
+sudo btrfs subvolume snapshot /mnt/res/@_clean /mnt/res/@
 sudo btrfs subvolume list /mnt/res
 sudo umount /mnt/res
 sudo rm -rf /mnt/res
